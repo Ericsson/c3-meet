364735c72ea225c8acbfe4cb14f083463b1c0485
Patrik Oldsberg <patrik.oldsberg@ericsson.com>
2017-08-23 13:53:05 +0200

    constants: use demo turn server, tcp and udp

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -14,30 +14,15 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-export const ICE_SERVERS = [
-  {
-    urls: 'stun:mmt-stun.verkstad.net',
-  },
-  {
-    urls: 'turn:static.verkstad.net:443?transport=tcp',
-    username: 'openwebrtc',
-    credential: 'secret',
-  },
-  {urls: 'stun:23.21.150.121'},
-  {urls:'stun:stun1.l.google.com:19302'},
-  {urls:'stun:stun2.l.google.com:19302'},
-  {urls:'stun:stun3.l.google.com:19302'},
-  {
-    urls: 'TURN:192.75.89.50:3478?transport=udp',
-    credential : 'ericsson246',
-    username: 'ericsson246',
-  },
-  {
-    urls: 'TURN:192.75.89.50:3478?transport=tcp',
-    credential : 'ericsson246',
-    username: 'ericsson246',
-  },
-]
+export const ICE_SERVERS = [{
+  urls: 'turn:turn.demo.c3.ericsson.net:443?transport=udp',
+  username: 'c3-turn',
+  credential: 'see-three',
+}, {
+  urls: 'turn:turn.demo.c3.ericsson.net:443?transport=tcp',
+  username: 'c3-turn',
+  credential: 'see-three',
+}]
 
 export const AUTH_OPTS = {
   serverUrl: 'https://demo.c3.ericsson.net',
