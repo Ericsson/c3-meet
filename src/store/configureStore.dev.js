24cf93cc8d643ad9fbf8a4738e2fbcbeac78e8ab
Patrik Oldsberg <patrik.oldsberg@ericsson.com>
2017-08-29 14:58:56 +0200

    store/configureStore.dev: use new way of importing createLogger

--- src/store/configureStore.dev.js
+++ src/store/configureStore.dev.js
@@ -16,7 +16,7 @@ limitations under the License.
 
 import {createStore, applyMiddleware, compose} from 'redux'
 import thunk from 'redux-thunk'
-import createLogger from 'redux-logger'
+import {createLogger} from 'redux-logger'
 import rootReducer from 'reducers'
 import DevTools from 'containers/DevTools'
 
