051024cd3f4eb8c670726d864faaa1fed9e72d3b
Patrik Oldsberg <patrik.oldsberg@ericsson.com>
2017-09-06 09:53:44 +0200

    redux: added a bunch of new client actions and reducers

--- src/actions/client.js
+++ src/actions/client.js
@@ -14,16 +14,66 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import {SET_CLIENT, UPDATE_OWN_USER, UPDATE_CLIENT_CONNECTION_STATE} from 'actions/constants'
+import {
+  SET_CLIENT,
+  SET_STORED_DISPLAY_NAME,
+  UPDATE_OWN_USER,
+  UPDATE_DISPLAY_NAME,
+  UPDATE_CLIENT_CONNECTION_STATE,
+  AUTHENTICATE_CLIENT_STARTED,
+  AUTHENTICATE_CLIENT_COMPLETE,
+  AUTHENTICATE_CLIENT_FAILED,
+  SET_DISPLAY_NAME_STARTED,
+  SET_DISPLAY_NAME_COMPLETE,
+  SET_DISPLAY_NAME_FAILED,
+} from 'actions/constants'
+
+import {clientAnonymousAuth, setDisplayName, getStoredDisplayName} from 'modules/auth'
 
 export function setClient(client) {
   return {type: SET_CLIENT, client}
+}
+
+export function loadStoredDisplayName() {
+  let storedDisplayName = getStoredDisplayName()
+  return {type: SET_STORED_DISPLAY_NAME, storedDisplayName}
 }
 
 export function updateOwnUser(ownUser) {
   return {type: UPDATE_OWN_USER, ownUser}
 }
 
+export function updateDisplayName(displayName) {
+  return {type: UPDATE_DISPLAY_NAME, displayName}
+}
+
 export function updateClientConnectionState(connectionState) {
   return {type: UPDATE_CLIENT_CONNECTION_STATE, connectionState}
+}
+
+export function authenticateClient(client) {
+  return (dispatch, getState) => {
+    let {client} = getState().client
+
+    dispatch({type: AUTHENTICATE_CLIENT_STARTED})
+    clientAnonymousAuth({client}).then(() => {
+      dispatch({type: AUTHENTICATE_CLIENT_COMPLETE})
+    }).catch(error => {
+      dispatch({type: AUTHENTICATE_CLIENT_FAILED, error})
+    })
+  }
+}
+
+export function setClientDisplayName(displayName = null) {
+  return (dispatch, getState) => {
+    let {client} = getState().client
+
+    dispatch({type: SET_DISPLAY_NAME_STARTED, displayName})
+
+    setDisplayName({client, displayName}).then(() => {
+      dispatch({type: SET_DISPLAY_NAME_COMPLETE, displayName})
+    }).catch(error => {
+      dispatch({type: SET_DISPLAY_NAME_FAILED, error})
+    })
+  }
 }
--- src/reducers/client.js
+++ src/reducers/client.js
@@ -1,23 +1,74 @@
 
-import {SET_CLIENT, UPDATE_OWN_USER, UPDATE_CLIENT_CONNECTION_STATE} from 'actions'
+import {
+  SET_CLIENT,
+  SET_STORED_DISPLAY_NAME,
+  UPDATE_OWN_USER,
+  UPDATE_DISPLAY_NAME,
+  UPDATE_CLIENT_CONNECTION_STATE,
+  AUTHENTICATE_CLIENT_STARTED,
+  AUTHENTICATE_CLIENT_COMPLETE,
+  AUTHENTICATE_CLIENT_FAILED,
+  SET_DISPLAY_NAME_STARTED,
+  SET_DISPLAY_NAME_COMPLETE,
+  SET_DISPLAY_NAME_FAILED,
+} from 'actions/constants'
 
 const initialState = {
   client: null,
   ownUser: null,
   connectionState: null,
+  displayName: null,
+  storedDisplayName: null,
+
+  authenticateClientInProgress: false,
+  authenticateClientError: null,
+
+  setDisplayNameInProgress: false,
+  setDisplayNameError: null,
 }
 
 export default function client(state = initialState, action) {
   switch (action.type) {
-    case SET_CLIENT:
+    case SET_CLIENT: {
       let {client} = action
       return {...state, client}
-    case UPDATE_OWN_USER:
+    }
+    case SET_STORED_DISPLAY_NAME: {
+      let {storedDisplayName} = action
+      return {...state, storedDisplayName}
+    }
+    case UPDATE_OWN_USER: {
       let {ownUser} = action
       return {...state, ownUser}
-    case UPDATE_CLIENT_CONNECTION_STATE:
+    }
+    case UPDATE_DISPLAY_NAME: {
+      let {displayName} = action
+      return {...state, displayName}
+    }
+    case UPDATE_CLIENT_CONNECTION_STATE: {
       let {connectionState} = action
       return {...state, connectionState}
+    }
+    case AUTHENTICATE_CLIENT_STARTED: {
+      return {...state, authenticateClientInProgress: true}
+    }
+    case AUTHENTICATE_CLIENT_COMPLETE: {
+      return {...state, authenticateClientInProgress: false, authenticateClientError: null}
+    }
+    case AUTHENTICATE_CLIENT_FAILED: {
+      let {error} = action
+      return {...state, authenticateClientInProgress: false, authenticateClientError: error}
+    }
+    case SET_DISPLAY_NAME_STARTED: {
+      return {...state, setDisplayNameInProgress: true}
+    }
+    case SET_DISPLAY_NAME_COMPLETE: {
+      return {...state, setDisplayNameInProgress: false, setDisplayNameError: null}
+    }
+    case SET_DISPLAY_NAME_FAILED: {
+      let {error} = action
+      return {...state, setDisplayNameInProgress: false, setDisplayNameError: error}
+    }
     default:
       return state
   }
