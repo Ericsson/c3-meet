4dfa03d9c69cdcd43b10a0499980ca6db72a66ec
Patrik Oldsberg <patrik.oldsberg@ericsson.com>
2017-09-09 22:44:47 +0200

    store/configureClient: set log levels and enable log colors

--- src/store/configureClient.js
+++ src/store/configureClient.js
@@ -1,6 +1,10 @@
 
-import {iceServers} from 'modules/config'
-import {Client} from '@cct/libcct'
+import {iceServers, logLevel, appLogLevel, LOG_TAG} from 'modules/config'
+import {Client, log} from '@cct/libcct'
+
+log.color = true
+log.setLogLevel(logLevel)
+log.setLogLevel(LOG_TAG, appLogLevel)
 
 export default function configureClient() {
   let client = new Client({iceServers})
