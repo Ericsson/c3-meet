27f7e41040427c0f52923eea3071fb05cb3ae199
Patrik Oldsberg <patrik.oldsberg@ericsson.com>
2017-09-11 22:30:51 +0200

    containers: added MeetingHistory

---
+++ src/containers/MeetingHistory.js
@@ -0,0 +1,69 @@
+/*
+Copyright 2016 Ericsson AB.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+import {connect} from 'react-redux'
+import {withRouter} from 'react-router-dom'
+import classNames from 'classnames'
+import {X} from 'react-feather'
+
+import {removeMeetingById} from 'actions/meetingHistory'
+
+import Button from 'components/Button'
+import MeetingList from 'components/MeetingList'
+
+import {formatRelative} from 'modules/intl'
+
+import styles from './MeetingHistory.css'
+
+class MeetingHistory extends Component {
+  constructor(props) {
+    super(props)
+    this.handleEnter = this.handleEnter.bind(this)
+  }
+
+  handleEnter(meetingName) {
+    this.props.history.push(`/${meetingName}`)
+  }
+
+  render() {
+    let {className, ...props} = this.props
+    return (
+      <MeetingList
+        className={classNames(styles.list, className)}
+        onEnter={this.handleEnter}
+        {...props}/>
+    )
+  }
+}
+
+MeetingHistory.propTypes = {
+  history: PropTypes.object.isRequired,
+  ...MeetingList.propTypes,
+}
+
+const mapStateToProps = state => ({
+  meetings: state.meetingHistory.meetings,
+  orderBy: state.meetingHistory.orderBy,
+  descending: state.meetingHistory.descending,
+})
+
+const mapDispatchToProps = dispatch => ({
+  onRemove: meetingId => dispatch(removeMeetingById(meetingId)),
+})
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(MeetingHistory))
