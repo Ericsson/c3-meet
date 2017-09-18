086dd131a7acef4dfeb73fd4cfb15207eff15ce7
Patrik Oldsberg <patrik.oldsberg@ericsson.com>
2017-09-18 17:11:47 +0200

    store/configureClient: properly forward statsUrl

--- src/store/configureClient.js
+++ src/store/configureClient.js
@@ -1,5 +1,5 @@
 
-import {iceServers, logLevel, appLogLevel, LOG_TAG} from 'modules/config'
+import {iceServers, statsUrl, logLevel, appLogLevel, LOG_TAG} from 'modules/config'
 import {Client, log} from '@cct/libcct'
 
 log.color = true
@@ -7,7 +7,7 @@ log.setLogLevel(logLevel)
 log.setLogLevel(LOG_TAG, appLogLevel)
 
 export default function configureClient() {
-  let client = new Client({iceServers})
+  let client = new Client({iceServers, statsUrl})
   window.client = client
   return client
 }
