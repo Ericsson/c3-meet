17e428138802edb4ecf9d3107c7ec0fe716e19cd
Patrik Oldsberg <patrik.oldsberg@ericsson.com>
2017-10-24 10:03:13 +0200

    components: added AudioDevicePreview and VideoDevicePreview

---
+++ src/components/AudioDevicePreview.js
@@ -0,0 +1,87 @@
+/*
+Copyright 2017 Ericsson AB.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+import {DeviceSource, WaveformView, browserInfo} from '@cct/libcct'
+
+import styles from './AudioDevicePreview.css'
+
+class AudioDevicePreview extends Component {
+  constructor(props) {
+    super(props)
+    this.handleCanvasRef = this.handleCanvasRef.bind(this)
+
+    this.waveformView = new WaveformView()
+  }
+
+  componentWillMount(props = this.props) {
+    if (browserInfo.browser === 'firefox') {
+      if (props.checked) {
+        this.startPreview(props.device)
+      }
+    } else {
+      this.startPreview(props.device)
+    }
+  }
+
+  componentWillUnmount() {
+    this.stopPreview()
+  }
+
+  componentWillReceiveProps(newProps) {
+    if (browserInfo.browser === 'firefox' && this.props.checked !== newProps.checked) {
+      this.stopPreview()
+      this.componentWillMount(newProps)
+    }
+  }
+
+  startPreview(device) {
+    this.source = new DeviceSource({audio: {device}})
+    this.source.connect(this.waveformView)
+    this.source.promise.catch(console.error)
+  }
+
+  stopPreview() {
+    if (this.source) {
+      this.source.disconnect(this.waveformView)
+      this.source.stop()
+      this.source = null
+      if (this.canvas) {
+        let context = this.canvas.getContext('2d')
+        let {width, height} = this.canvas
+        context.clearRect(0, 0, width, height)
+      }
+    }
+  }
+
+  handleCanvasRef(canvas) {
+    this.canvas = canvas
+    if (canvas) {
+      canvas.width = 160
+      canvas.height = 45
+      let context = canvas.getContext('2d')
+      context.lineWidth = 0.5
+    }
+    this.waveformView.canvas = canvas
+  }
+
+  render() {
+    return <canvas className={styles.waveform} ref={this.handleCanvasRef}/>
+  }
+}
+
+export default AudioDevicePreview
---
+++ src/components/VideoDevicePreview.js
@@ -0,0 +1,45 @@
+/*
+Copyright 2017 Ericsson AB.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+import {DeviceSource} from '@cct/libcct'
+
+import Video from 'components/Video'
+
+import styles from './VideoDevicePreview.css'
+
+class VideoDevicePreview extends Component {
+  componentWillMount() {
+    this.source = new DeviceSource({video: {
+      device: {exact: this.props.device},
+      aspectRatio: 16/9,
+      width: 160,
+      height: 90,
+    }})
+  }
+
+  componentWillUnmount() {
+    this.source.stop()
+    this.source = null
+  }
+
+  render() {
+    return <Video className={styles.video} source={this.source}/>
+  }
+}
+
+export default VideoDevicePreview
